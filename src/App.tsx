64591814f998a393a3f54cc283017b6ba1a70d0a
doyoung.kim <59991222+doyoungrong@users.noreply.github.com>
2026-02-05 21:42:54 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
 import InvitationSvg from "./components/InvitationSvg";
 
 export default function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        background: "#fff",
-      }}
-    >
-      <InvitationSvg />
-    </div>
-  );
+  return <InvitationSvg />;
 }
