37c12f654fb1a32ecaaabd5f817f821a19ca39b0
doyoung.kim <59991222+doyoungrong@users.noreply.github.com>
2026-02-05 22:16:29 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // ✅ 이게 없어서 CSS가 적용이 안 됐던 거예요
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
