c6a11b9d4522c6ec0edf36dab37b2e32a1ec4079
doyoung.kim <59991222+doyoungrong@users.noreply.github.com>
2026-02-07 01:39:48 +0900

    Create WeSection.tsx

---
+++ src/components/WeSection.tsx
@@ -0,0 +1,9 @@
+import weSvg from "../assets/We.svg";
+
+export default function WeSection() {
+  return (
+    <div className="invitation">
+      <img src={weSvg} alt="We" className="invitation-img" />
+    </div>
+  );
+}
