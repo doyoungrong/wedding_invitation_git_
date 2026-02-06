8b353ab8d4fd12089d346bbfd7614f972d83fff7
doyoung.kim <59991222+doyoungrong@users.noreply.github.com>
2026-02-07 03:00:37 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import MainWithMusic from "./components/MainWithMusic";
 import InvitationSvg from "./components/InvitationSvg";
 import YoutubeSection from "./components/YoutubeSection";
 import WeSection from "./components/WeSection";
+import GallerySection from "./components/GallerySection";
 
 export default function App() {
   return (
     <div className="page">
-      {/* Main 섹션 */}
       <div className="invitation">
         <MainWithMusic />
       </div>
 
-      {/* YouTube 섹션 */}
       <YoutubeSection videoId="TM_SaCao0bI" />
 
-      {/* ✅ We 섹션 (영상 바로 아래) */}
       <WeSection />
 
-      {/* Invitation 섹션 */}
+      {/* ✅ We.svg 바로 다음: 갤러리 */}
+      <GallerySection />
+
       <InvitationSvg />
     </div>
   );
