59e69fbfee0aeb80ca9dd474c9d2bc1414eb3825
doyoung.kim <59991222+doyoungrong@users.noreply.github.com>
2026-02-08 15:20:45 +0900

    Create MapSection.tsx

---
+++ src/components/MapSection.tsx
@@ -0,0 +1,9 @@
+import mapSvg from "../assets/Map.svg";
+
+export default function MapSection() {
+  return (
+    <div className="invitation">
+      <img src={mapSvg} alt="Map" className="invitation-img" />
+    </div>
+  );
+}
